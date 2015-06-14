6979e3ac0fbc89a676d3a0987055d7a7623bcd5a
ziang <yanziang1994@sina.com>
2015-06-14 16:50:42 +0800

    finish learnyounode http collect using bl module

--- nodejs/learnyounode/program.js
+++ nodejs/learnyounode/program.js
@@ -1,8 +1,14 @@
 var path = require('path');
 var http = require('http');
+var bl = require('bl');
 
 http.get(process.argv[2], function (response) {
     response.setEncoding('utf8');
-    response.on("data", console.log);
+    response.pipe(bl( function (err, data) {
+    if (err) return console.error(err);
+    data = data.toString();
+    console.log(data.length);
+    console.log(data);
+    }));
 });
 
