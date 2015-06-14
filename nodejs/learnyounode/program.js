e1bde84540755338402bc32ae308c898377426fb
ziang <yanziang1994@sina.com>
2015-06-14 17:06:15 +0800

    finish learnyounode async

--- nodejs/learnyounode/program.js
+++ nodejs/learnyounode/program.js
@@ -1,14 +1,27 @@
 var path = require('path');
 var http = require('http');
 var bl = require('bl');
+var results = [];
+var count = 0;
 
-http.get(process.argv[2], function (response) {
-    response.setEncoding('utf8');
-    response.pipe(bl( function (err, data) {
-    if (err) return console.error(err);
-    data = data.toString();
-    console.log(data.length);
-    console.log(data);
-    }));
-});
+function printResult() {
+    for(var i = 0; i < 3; ++i){
+        console.log(results[i]);
+    }
+}
 
+function httpGet(index) {
+    http.get(process.argv[2 + index], function (response) {
+        response.pipe(bl(function (err, data) {
+            if (err)
+                return console.error(err);
+            results[index] = data.toString();
+            count++;
+            if (count == 3)
+                printResult();
+        }));
+    });
+}
+
+for (var i = 0; i < 3; ++i)
+    httpGet(i); 
