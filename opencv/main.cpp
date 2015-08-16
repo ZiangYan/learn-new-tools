40e0b1ee5bd51fe870d97cb164ca68bf5f3c04e6
ziang <yanziang1994@sina.com>
2015-08-16 18:31:41 +0800

    opencv lut

--- opencv/main.cpp
+++ opencv/main.cpp
@@ -4,11 +4,16 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+void changeMat(cv::Mat M)
+{
+    uchar *p = M.ptr<uchar>(0);
+    p[0] = 23;
+}
 
 int main(int argc, char* argv[])
 {
-    cv::Mat A(600, 800, CV_8UC1);
-    cv::Mat B(600, 800, CV_8UC3);
+    cv::Mat A(5, 5, CV_8UC1);
+    cv::Mat B(5, 5, CV_8UC3);
 
     for(int h = 0; h < A.rows; ++h){
         uchar *p = A.ptr<uchar>(h);
@@ -18,6 +23,17 @@ int main(int argc, char* argv[])
             p[w] = (h+w) % 255;
         }
     }
+    cv::Mat C = A(cv::Range::all(), cv::Range(1, 3));
+    for(int h = 0; h < C.rows; ++h){
+        uchar *p = C.ptr<uchar>(h);
+        for(int w = 0; w < C.cols; ++w){
+            p[w] = 100;
+        }
+    }
+    uchar *pp = C.data;
+    pp[0] = 111;
+    pp[1] = 111;
+    pp[2] = 111;
     for(int h = 0; h < B.rows; ++h){
         cv::Vec3b *p = B.ptr<cv::Vec3b>(h);
         for(int w = 0; w < B.cols; ++w){
@@ -33,12 +49,23 @@ int main(int argc, char* argv[])
             p[w][2] = 0;
         }
     }
-    std::cout << B(cv::Range::all(), cv::Range(1, 2)) << std::endl;
-    std::cout << B(cv::Rect(10, 10, 100, 100)) << std::endl;
-    cv::imshow("gray image", A);
-    cv::imshow("color image", B);
-    cv::waitKey(0);
+    changeMat(A);
+    cv::Mat lut(1, 256, CV_8U);
+    uchar *p = lut.data;
+    for(int i = 0; i < 256; ++i){
+        if(i != 4) 
+            p[i] = i;
+        else
+            p[i] = 45;
+    }
+    cv::Mat D;
+    cv::LUT(A, lut, D);
+    std::cout << A << std::endl;
+    std::cout << C << std::endl;
+    std::cout << D << std::endl;
+    //cv::imshow("gray image", A);
+    //cv::imshow("color image", B);
+    //cv::waitKey(0);
 
-    int a = 0;
-    return a;
+    return 0;
 }
