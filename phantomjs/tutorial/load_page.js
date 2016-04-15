5cd4dd22be1172e5a88999324b6906c708b639a9
yanziang <yanziang1994@sina.com>
2016-04-15 18:30:19 +0800

    phantomjs render web pages

---
+++ phantomjs/tutorial/load_page.js
@@ -0,0 +1,9 @@
+var page = require('webpage').create();
+page.open('https://www.taobao.com', function(status) {
+    console.log('status: ' + status);
+    if (status == 'success') {
+        page.render('example.png');
+    }
+
+    phantom.exit();
+});
