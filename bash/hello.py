9d9fd4a8fa6dea7a6990720819a8628d1f54b424
yanziang <yanziang1994@sina.com>
2016-07-13 12:27:58 +0800

    nothing

--- bash/hello.py
+++ bash/hello.py
@@ -13,11 +13,12 @@ for line in output:
         new_line = line.replace('  ', ' ')
     #print line
     name, pts = line.split(' ')[:2]
-    if name == 'yanziang' or name == 'liuhu':
+    #if name == 'yanziang' or name == 'liuhu':
+    if True:
+        shell_cmd = 'echo "%s" > /dev/%s' % (msg, pts)
+        os.system(shell_cmd)
         shell_cmd = 'sudo ttyecho -n /dev/%s "/usr/games/sl"' % pts
         os.system(shell_cmd)
         date = commands.getoutput("date +'%H:%M'")
         msg = '三点多了，大家起来休息一下 (%s) --by kalen wu' % date
-        shell_cmd = 'echo "%s" > /dev/%s' % (msg, pts)
-        os.system(shell_cmd)
 
